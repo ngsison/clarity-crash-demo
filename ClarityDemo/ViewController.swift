377186ed401b7b28ea2244a37e637cd9dc9b6add
Nathaniel Brion Sison <ngsison@outlook.com>
2025-01-20 17:13:11 +0800

    Initial Commit

--- ClarityDemo/ViewController.swift
+++ ClarityDemo/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var button: UIButton = {
+        let button = UIButton(configuration: .filled())
+        button.setTitle("Button", for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc
+    func buttonTapped() {
+        let popup = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Popup")
+        present(popup, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(120)
+            make.height.equalTo(44)
+        }
     }
-
-
 }
-
